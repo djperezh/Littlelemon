f430ce1fc1ccf95bf0d74324b20a45eb3ea1f1e7
Dom Perez <dom.perez@climate.com>
2023-08-31 14:47:58 -0700

    Include category and implement throttling

--- LittleLemonDRF/urls.py
+++ LittleLemonDRF/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('category', views.CategoriesView.as_view()),
+    path('category', views.CategoriesView.as_view()),
     path('menu-items', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('throttle-check', views.throttle_check),
+    path('throttle-check-auth', views.throttle_check_auth),
 ]
--- LittleLemonDRF/views.py
+++ LittleLemonDRF/views.py
@@ -1,10 +1,14 @@
 from rest_framework import generics
-from .models import MenuItem #, Category
-from .serializers import MenuItemSerializer #, CategorySerializer
+from .models import MenuItem, Category
+from .serializers import MenuItemSerializer, CategorySerializer
+from rest_framework.decorators import api_view, throttle_classes
+from rest_framework.response import Response
+from rest_framework.throttling import AnonRateThrottle
+from rest_framework.throttling import UserRateThrottle
 
-# class CategoriesView(generics.ListCreateAPIView):
-#     queryset = Category.objects.all()
-#     serializer_class = CategorySerializer
+class CategoriesView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
     
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
@@ -16,3 +20,13 @@ class MenuItemsView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.RetrieveDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    
+@api_view()
+@throttle_classes([AnonRateThrottle])
+def throttle_check(request):
+    return Response({"message":"successful"})
+
+@api_view()
+@throttle_classes([UserRateThrottle])
+def throttle_check_auth(request):
+    return Response({"message":"message for the logged user only"})
